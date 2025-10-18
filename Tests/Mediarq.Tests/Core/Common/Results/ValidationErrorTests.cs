f54ef1a13c1920b83befe3430a6aa34231f559ca
Nicolas Rouffart <rouffart.nicolas@gmail.com>
2025-10-18 21:04:37 +0200

    Updates ValidationErrorTests namespace
    Updates ValidationErrorTests to follow C# 10 namespace declaration.

--- Tests/Mediarq.Tests/Core/Common/Results/ValidationErrorTests.cs
+++ Tests/Mediarq.Tests/Core/Common/Results/ValidationErrorTests.cs
@@ -1,78 +1,77 @@
-namespace Mediarq.Tests.Core.Common.Results
+
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Mediarq.Core.Common.Results;
+using Xunit;
+
+namespace Mediarq.Tests.Core.Common.Results;
+public class ValidationErrorTests
 {
-    using System;
-    using System.Collections.Generic;
-    using FluentAssertions;
-    using Mediarq.Core.Common.Results;
-    using Xunit;
-
-    public class ValidationErrorTests
+    [Fact]
+    public void Should_Contain_All_Inner_Errors()
     {
-        [Fact]
-        public void Should_Contain_All_Inner_Errors()
+        // Arrange
+        var innerErrors = new List<Error>
         {
-            // Arrange
-            var innerErrors = new List<Error>
-            {
-                Error.Failure("Field1", "Error message 1"),
-                Error.Failure("Field2", "Error message 2"),
-                Error.Failure("Field3", "Error message 3")
-            };
+            Error.Failure("Field1", "Error message 1"),
+            Error.Failure("Field2", "Error message 2"),
+            Error.Failure("Field3", "Error message 3")
+        };
 
-            // Act
-            var validationError = new ValidationError(innerErrors.ToArray());
+        // Act
+        var validationError = new ValidationError(innerErrors.ToArray());
 
-            // Assert
-            validationError.Errors.Should().NotBeNull();
-            validationError.Errors.Should().HaveCount(3);
-            validationError.Errors.Should().BeEquivalentTo(innerErrors);
-        }
+        // Assert
+        validationError.Errors.Should().NotBeNull();
+        validationError.Errors.Should().HaveCount(3);
+        validationError.Errors.Should().BeEquivalentTo(innerErrors);
+    }
 
-        [Fact]
-        public void Should_Handle_Empty_Result()
+    [Fact]
+    public void Should_Handle_Empty_Result()
+    {
+        // Arrange
+        var results = Array.Empty<Result>();
+        // Act
+        var validationError = ValidationError.FromResults(results);
+        // Assert
+        validationError.Errors.Should().NotBeNull();
+        validationError.Errors.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Should_Create_ValidationError_From_Results()
+    {
+        // Arrange
+        var results = new List<Result>
         {
-            // Arrange
-            var results = Array.Empty<Result>();
-            // Act
-            var validationError = ValidationError.FromResults(results);
-            // Assert
-            validationError.Errors.Should().NotBeNull();
-            validationError.Errors.Should().BeEmpty();
-        }
+            Result.Success(),
+            Result.Failure(Error.Failure("Field1", "Error message 1")),
+            Result.Failure(Error.Failure("Field2", "Error message 2")),
+            Result.Success(),
+            Result.Failure(Error.Failure("Field3", "Error message 3"))
+        };
+        
+        // Act
+        var validationError = ValidationError.FromResults(results);
 
-        [Fact]
-        public void Should_Create_ValidationError_From_Results()
-        {
-            // Arrange
-            var results = new List<Result>
-            {
-                Result.Success(),
-                Result.Failure(Error.Failure("Field1", "Error message 1")),
-                Result.Failure(Error.Failure("Field2", "Error message 2")),
-                Result.Success(),
-                Result.Failure(Error.Failure("Field3", "Error message 3"))
-            };
-            
-            // Act
-            var validationError = ValidationError.FromResults(results);
+        // Assert
+        validationError.Errors.Should().NotBeNull();
+        validationError.Errors.Should().HaveCount(3);
+        validationError.Errors.Should().Contain(e => e.Code == "Field1" && e.Message == "Error message 1");
+        validationError.Errors.Should().Contain(e => e.Code == "Field2" && e.Message == "Error message 2");
+        validationError.Errors.Should().Contain(e => e.Code == "Field3" && e.Message == "Error message 3");
+    }
 
-            // Assert
-            validationError.Errors.Should().NotBeNull();
-            validationError.Errors.Should().HaveCount(3);
-            validationError.Errors.Should().Contain(e => e.Code == "Field1" && e.Message == "Error message 1");
-            validationError.Errors.Should().Contain(e => e.Code == "Field2" && e.Message == "Error message 2");
-            validationError.Errors.Should().Contain(e => e.Code == "Field3" && e.Message == "Error message 3");
-        }
-
-        [Fact]
-        public void Constructor_Should_Throw_ArgumentNullException_When_Errors_Is_Null()
-        {            
-            // Act
-            Action act = () => new ValidationError(null);
-            
-            // Assert
-            act.Should().Throw<ArgumentNullException>()
-                .WithMessage("*errors*");
-        }
+    [Fact]
+    public void Constructor_Should_Throw_ArgumentNullException_When_Errors_Is_Null()
+    {            
+        // Act
+        Action act = () => new ValidationError(null);
+        
+        // Assert
+        act.Should().Throw<ArgumentNullException>()
+            .WithMessage("*errors*");
     }
 }
